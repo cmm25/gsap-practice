0855b14963cb22df4093c60b0bbf016d3049dab0
CRAIG MUTEGI MUTUGI <craigmutegi@gmail.com>
2024-01-09 02:53:54 +0300

    heavy styling

--- src/Gallery/gallery.js
+++ src/Gallery/gallery.js
@@ -39,33 +39,49 @@ function GalleryItem({
     updateActiveImage,
     index,
 }){
-    return <div className="gallery-item-wrapper">
-        <div className="gallery-item">
+    return(
+    <div className="gallery-item-wrapper">
+        <div></div>
+        <div className={"gallery-item"}>
             <div className="gallery-item-info">
-                
+                <h1 className="gallery-info-title">{title}</h1>
+                <h2 className="gallery-info-subtitle">{subtitle}</h2>
+                <p className="gallery-info-category">{category}</p>
             </div>
-            <div className="gallery-item-image">
-                
-            </div>
-            
+            <div
+                className="gallery-item-image"
+                style={{ backgroundImage: `url(${src})` }}
+            ></div>
         </div>
-    </div>
-}
+        <div></div>
+  </div>
+);}
     
 
-export default function Gallery(){
+export default function Gallery({ src, index, columnOffset }){
     const[activeImage, setActiveImage]= useState(1);
+    const handleUpdateActiveImage = (index) => {
+        setActiveImage(index + 1);
+      };
 
     return(
-        <section className="section-wrapper gallery-wrap">
-            <div className="gallery">
-                <div className="gallery-counter"></div>
-                {images.map((image,index)=>(
-                    <GalleryItem key={src} index={index} {...image} updateActiveImage={(index)=> setActiveImage(index+1)}
-                     />
-                 ))}
+        <section data-scroll-section className="section-wrapper gallery-wrap">
+    
+          <div className="gallery" >
+            <div className="gallery-counter">
+              <span>{activeImage}</span>
+              <span className="divider" />
+              <span>{images.length}</span>
             </div>
-            
+            {images.map((image, index) => (
+              <GalleryItem
+                key={src}
+                index={index}
+                {...image}
+                updateActiveImage={handleUpdateActiveImage}
+              />
+            ))}
+          </div>
         </section>
-    )
+    );
 }
