5afbdd6628f49f6b74e3a41016c894c5c1d1895d
CRAIG MUTEGI MUTUGI <craigmutegi@gmail.com>
2024-01-08 11:32:00 +0300

    featured photos

---
+++ src/featured/featured.js
@@ -0,0 +1,15 @@
+import "./featured.css";
+export default function featured(){
+    const[]=photos;
+    return(
+        <section className="featured-section">
+            <div className="row-layout">
+                <h6>green</h6>
+            </div>
+            <div className="column-layout">
+                
+            </div>
+            
+        </section>
+    )
+}
