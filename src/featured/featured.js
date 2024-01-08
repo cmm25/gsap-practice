d6fe52c23cfe320a92f5409999ea47301f583750
CRAIG MUTEGI MUTUGI <craigmutegi@gmail.com>
2024-01-08 12:46:38 +0300

    about section

--- src/featured/featured.js
+++ src/featured/featured.js
@@ -3,14 +3,14 @@ import photos from "../data";
 export default function Feature(){
     const[firstUrl, secondUrl]= photos;
     return(
-        <section className="featured-section">
-            <div className="row-layout">
+        <section className="featured-section"  data-scroll-section>
+            <div className="row-layout" >
                 <h6>green</h6>
-                <img src={firstUrl} alt=""/>
+                <img src={firstUrl} alt="" data-scroll/>
             </div>
             <div className="column-layout">
                 <h6>lily</h6>
-                <img src={secondUrl} alt=""/>
+                <img src={secondUrl} alt="" data-scroll/>
             </div>
         </section>
     )
