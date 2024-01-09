071a125f54d5e737ac0853f8ae0cf11aaa2c2b06
CRAIG MUTEGI MUTUGI <craigmutegi@gmail.com>
2024-01-09 03:05:23 +0300

    FOOTER

--- src/App.js
+++ src/App.js
@@ -4,18 +4,23 @@ import About from './about-section/about';
 import CustomCursor from './CustomCursor';
 import Feature from './featured/featured';
 import Gallery from './Gallery/gallery';
+import Footer from './Footer/footer';
 import './App.css';
 function App() {
   return (
     <>
       <CustomCursor/>
-      <div className="main-container" id='main-container'>
-      <NavBar/>
+      <div
+      className="main-container"
+      id="main-container"
+      data-scroll-container>
+      <NavBar />
       <Header />
-      <Feature/>
-      <About/>
-      <Gallery/>
-      </div>
+      <Feature />
+      <About />
+      <Gallery />
+      <Footer />
+    </div>
     </>
   );
 }
---
+++ src/Footer/footer.js
@@ -0,0 +1,10 @@
+import SectionHeader from "../SectionHeader";
+import "./footer.css";
+export default function Footer(){
+    return <section className="footer">
+        <SectionHeader title="Made in"/>
+        <h1 className="Location">
+        Nairobi
+        </h1>
+    </section>
+}
