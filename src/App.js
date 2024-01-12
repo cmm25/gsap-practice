e507f05598beffc5226f56e5f27795d52bbc2c0c
CRAIG MUTEGI MUTUGI <craigmutegi@gmail.com>
2024-01-12 04:46:32 +0300

    styling

--- src/App.js
+++ src/App.js
@@ -9,27 +9,45 @@ import Footer from './Footer/footer';
 import useLocoScroll from './hooks/useLocalScroll';
 import './App.css';
 function App() {
-  const[preloader, setPreloader]=useState(true)
-  useLocoScroll(!preloader)
-  const[timer, setTimer]=useState(3)
-  const id=useRef(null);
-  const clear = () => {
-    window.clearInterval(id.current);
-    setPreloader(false);
-  };
-
-  useEffect(() => {
-    id.current = window.setInterval(() => {
-      setTimer((time) => time - 1);
-    }, 1000);
-    return () => clear();
-  }, []);
-
-  useEffect(() => {
-    if (timer === 0) {
-      clear();
+    const ref = useRef(null);
+    const [preloader, setPreload] = useState(true);
+  
+  
+    useLocoScroll(!preloader);
+  
+    useEffect(() => {
+      if (!preloader && ref) {
+        if (typeof window === "undefined" || !window.document) {
+          return;
+        }
+      }
+    }, [preloader]);
+  
+    const [timer, setTimer] = useState(3);
+  
+    const id = useRef(null);
+  
+    const clear = () => {
+      window.clearInterval(id.current);
+      setPreload(false);
+    };
+  
+    useEffect(() => {
+      id.current = window.setInterval(() => {
+        setTimer((time) => time - 1);
+      }, 1000);
+      return () => clear();
+    }, []);
+  
+   useEffect(() => {
+      if (timer === 0) {
+        clear();
+      }
+    }, [timer]);
+  
+    if (typeof window === "undefined" || !window.document) {
+      return null;
     }
-  }, [timer]);
   return (
     <>
       <CustomCursor />
