b9d9ee5bfd23b4a3457b01f6eb422d69926008a5
CRAIG MUTEGI MUTUGI <craigmutegi@gmail.com>
2024-01-13 14:24:35 +0300

    about section animation

--- src/about-section/about.js
+++ src/about-section/about.js
@@ -1,11 +1,40 @@
+import { useEffect, useRef, useState } from "react";
+import gsap from "gsap";
 import "./about.css";
 import SectionHeader from "../SectionHeader";
+import useOnScreen from "../hooks/useOnScreen";
+import SplitText from "../utils/Split3.min";
+import cn from 'classnames';
 
 export default function About(){
+    const ref = useRef(null);
+
+    const [reveal, setReveal] = useState(false);
+    const onScreen = useOnScreen(ref);
+  
+    useEffect(() => {
+      if (onScreen) setReveal(onScreen);
+    }, [onScreen]);
+    useEffect(() => {
+        if(reveal){
+        const split = new SplitText("#headline", {
+            type: "lines",
+            
+            });      
+          
+            gsap.to(split.lines, {
+            duration: 1,
+            y: -20,
+            opacity: 1,
+            stagger: 0.1,
+            ease: "power2",
+            });
+    }
+      }, [reveal]);
     return(
-        <section className="about-section" data-scroll-section>
-          <SectionHeader title={'about'}/>
-          <p id="headline">
+        <section  className={cn("about-section", { "is-reveal": reveal })} data-scroll-section>
+          <SectionHeader title='about'/>
+          <p ref={ref} id="headline" className={cn({ "is-reveal": reveal })}>
             Flirty Flowers is a blog about flowers and the floral designers who make
             them into art. Creativity and the art of ‘making’ require dialogue. The
             full purpose of the Flirty Flowers blog is to encourage and inspire. We
